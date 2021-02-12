7c77373fcca606bb5787453a8f6f63b1a0f8aed0
R. Allan White <allan.white@contentful.com>
2021-02-11 23:11:54 -0800

    removed webp because of Safari;(. Will polyfill someday.

--- components/avatar.js
+++ components/avatar.js
@@ -2,7 +2,7 @@ export default function Avatar({ name, picture }) {
   return (
     <div className="flex items-center">
       <img
-        src={`${picture.url}?w=96&h=96&fit=thumb&f=face&fm=webp&q=30`}
+        src={`${picture.url}?w=96&h=96&fit=thumb&f=face&fm=jpg&q=50`}
         className="w-12 h-12 rounded-full mr-4"
         alt={name}
       />
--- components/cover-image.js
+++ components/cover-image.js
@@ -16,15 +16,15 @@ export default function CoverImage({ title, url, slug }) {
   const image = (
     <picture>
     <source
-      srcSet={`${url}?w=1600&h=800&fit=thumb&fm=webp&q=40 1x, ${url}?w=2400&h=1200&fit=thumb&fm=webp&q=50 2x`}
-      src={`${url}?w=1200&h=600&fit=thumb&fm=webp&q=5`}
+      srcSet={`${url}?w=1600&h=800&fit=thumb&fm=jpg&q=40 1x, ${url}?w=2400&h=1200&fit=thumb&fm=jpg&q=50 2x`}
+      src={`${url}?w=1200&h=600&fit=thumb&fm=jpg&q=35`}
       media='(min-width: 40em)'
       className={cn('shadow-small', {
         'hover:shadow-medium transition-shadow duration-200': slug,
       })}
     />
     <img
-      src={`${url}?w=1200&h=600&fit=thumb&fm=webp&q=5`}
+      src={`${url}?w=1200&h=600&fit=thumb&fm=jpg&q=40`}
       alt={`Cover Image for ${title}`}
       loading='lazy'
     />
