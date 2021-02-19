9dac2e19fdaa839bde4a4c32e1a23171c1ec708a
R. Allan White <allan.white@contentful.com>
2021-02-19 15:33:15 -0800

    Upgraded purgecss, configs
    CSS appears to be writing in the correct order, and more retained in the purgecss steps.

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,8 @@
 module.exports = {
   plugins: [
     'postcss-import',
-    'postcss-nested',
     'tailwindcss',
+    'postcss-nested',
     'autoprefixer',
-    process.env.NODE_ENV === 'production'
-      ? [
-          '@fullhuman/postcss-purgecss',
-          {
-            content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}',
-            ],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        ]
-      : undefined,
-    'postcss-preset-env',
   ],
 }
